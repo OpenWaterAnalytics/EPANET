8be1fcffd68a8d0ca3bbe294eacd1474e936bc0a
Elad Salomons <selad@optiwater.com>
2015-09-24 12:55:29 +0300

    Fix header file

--- include/epanet2.h
+++ include/epanet2.h
@@ -257,8 +257,8 @@ extern "C" {
   int  DLLEXPORT ENgetlinkvalue(int index, int code, EN_API_FLOAT_TYPE *value);
   
   int  DLLEXPORT ENgetcurve(int curveIndex, char* id, int *nValues, EN_API_FLOAT_TYPE **xValues, EN_API_FLOAT_TYPE **yValues);
-  int  DLLEXPORT ENgetheadcurve(int, char *);
-  int  DLLEXPORT ENgetpumptype(int, int *);
+  int  DLLEXPORT ENgetheadcurve(int index, char *id);
+  int  DLLEXPORT ENgetpumptype(int index, int *type);
   
   int  DLLEXPORT ENgetversion(int *version);
   
