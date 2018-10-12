93760176bfd1949173f263b0e65048651073fc5e
Michael Tryby <tryby.michael@epa.gov>
2018-10-12 08:58:17 -0400

    Updateing benchmark

--- tools/before-test.cmd
+++ tools/before-test.cmd
@@ -25,8 +25,8 @@ set SCRIPT_HOME=%~dp0
 set TEST_HOME=%~1
 
 
-set EXAMPLES_VER=1.0.2-dev.3
-set BENCHMARK_VER=220dev3
+set EXAMPLES_VER=1.0.2-dev.4
+set BENCHMARK_VER=220dev4
 
 
 set TESTFILES_URL=https://github.com/OpenWaterAnalytics/epanet-example-networks/archive/v%EXAMPLES_VER%.zip
