b34c2ede81d444ff0857a07da7161ea857cceb43
Michael Tryby <tryby.michael@epa.gov>
2018-11-16 14:06:29 -0500

    Fixing build error on Travis

--- tests/test_toolkit.cpp
+++ tests/test_toolkit.cpp
@@ -9,9 +9,13 @@
 
 //#define BOOST_TEST_DYN_LINK
 
+#ifdef _WIN32
 #define _CRTDBG_MAP_ALLOC
 #include <stdlib.h>
 #include <crtdbg.h>
+#else
+#include <stdlib.h>
+#endif
 
 #define BOOST_TEST_MODULE "toolkit"
 #include <boost/test/included/unit_test.hpp>
