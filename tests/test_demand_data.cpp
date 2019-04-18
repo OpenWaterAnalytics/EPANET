584233e5e88bb625aa80a68e3c0fa13bfb82d3d2
Michael Tryby <tryby.michael@epa.gov>
2019-04-18 09:54:56 -0400

    Create test_demand_data.cpp

---
+++ tests/test_demand_data.cpp
@@ -0,0 +1,25 @@
+/*
+ ******************************************************************************
+ Project:      OWA EPANET
+ Version:      2.2
+ Module:       /test_demand_data.cpp
+ Description:  tests demand data list node struct
+ Authors:      see AUTHORS
+ Copyright:    see AUTHORS
+ License:      see LICENSE
+ Last Updated: 04/18/2019
+ ******************************************************************************
+*/
+
+#define BOOST_TEST_MODULE demand_data
+#include <boost/test/unit_test.hpp>
+
+#include "demand.h"
+
+
+BOOST_AUTO_TEST_SUITE(test_filemanager)
+
+
+
+
+BOOST_AUTO_TEST_SUITE_END()
