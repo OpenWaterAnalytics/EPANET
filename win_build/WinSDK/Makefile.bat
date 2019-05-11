e80a833b852e88338c4d407b01f9901a08f9c67f
Elad Salomons <elad.salomons@gmail.com>
2019-05-11 16:00:27 +0300

    Remove duplicate DEF file
    Update win build to use the DEF file in the include folder

--- win_build/WinSDK/Makefile.bat
+++ win_build/WinSDK/Makefile.bat
@@ -35,7 +35,7 @@ rem : 32 bit with DEF
 CALL "%SDK_PATH%bin\"SetEnv.cmd /x86 /release
 echo "32 bit with epanet2.def mapping"
 rem : create EPANET2.DLL
-cl -o epanet2.dll epanet.c epanet2.c hash.c hydraul.c hydcoeffs.c hydstatus.c hydsolver.c inpfile.c input1.c input2.c input3.c mempool.c output.c project.c quality.c qualroute.c qualreact.c report.c rules.c smatrix.c genmmd.c /Depanet2_EXPORTS /I ..\include /I ..\run /link /DLL /def:..\win_build\WinSDK\epanet2.def /MAP
+cl -o epanet2.dll epanet.c epanet2.c hash.c hydraul.c hydcoeffs.c hydstatus.c hydsolver.c inpfile.c input1.c input2.c input3.c mempool.c output.c project.c quality.c qualroute.c qualreact.c report.c rules.c smatrix.c genmmd.c /Depanet2_EXPORTS /I ..\include /I ..\run /link /DLL /def:..\include\epanet2.def /MAP
 rem : create EPANET2.EXE
 cl -o epanet2.exe epanet.c epanet2.c ..\run\main.c hash.c hydraul.c hydcoeffs.c hydstatus.c hydsolver.c inpfile.c input1.c input2.c input3.c mempool.c output.c project.c quality.c qualroute.c qualreact.c report.c rules.c smatrix.c genmmd.c /Depanet2_EXPORTS /I ..\include /I ..\run /I ..\src /link
 md "%Build_PATH%"\32bit
