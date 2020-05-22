949022ed494ddee363f680c58566ddc7235e7971
james uber <james.uber@xyleminc.com>
2020-05-22 11:45:52 -0400

    API access to incontrols() for information about node/link controls participation
    allowing access to incontrols() for getting information about whether a node or link participates in a simple or rule-based control.

--- include/epanet2_enums.h
+++ include/epanet2_enums.h
@@ -63,7 +63,8 @@ typedef enum {
   EN_TANKVOLUME   = 24, //!< Current computed tank volume (read only)
   EN_MAXVOLUME    = 25, //!< Tank maximum volume (read only)
   EN_CANOVERFLOW  = 26, //!< Tank can overflow (= 1) or not (= 0)
-  EN_DEMANDDEFICIT = 27 //!< Amount that full demand is reduced under PDA (read only)
+  EN_DEMANDDEFICIT = 27,//!< Amount that full demand is reduced under PDA (read only)
+  EN_NODE_INCONTROL = 28  //!< Is present in any simple or rule-based control (= 1) or not (= 0)
 } EN_NodeProperty;
 
 /// Link properties
@@ -94,7 +95,8 @@ typedef enum {
   EN_PUMP_HCURVE  = 19, //!< Pump head v. flow curve index
   EN_PUMP_ECURVE  = 20, //!< Pump efficiency v. flow curve index
   EN_PUMP_ECOST   = 21, //!< Pump average energy price
-  EN_PUMP_EPAT    = 22  //!< Pump energy price time pattern index
+  EN_PUMP_EPAT    = 22, //!< Pump energy price time pattern index
+  EN_LINK_INCONTROL = 23  //!< Is present in any simple or rule-based control (= 1) or not (= 0)
 } EN_LinkProperty;
 
 /// Time parameters
